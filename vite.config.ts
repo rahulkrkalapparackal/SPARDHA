2899e18245248594e7d3196f45ba5e99b70cf364
Rahul K R <rahulkr101360@gmail.com>
2026-08-30 15:12:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SPARDHA/',
   plugins: [react()],
   resolve: {
     alias: {
